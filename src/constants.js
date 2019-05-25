10939814f3b8c2cea6986df4b3fda490cda4dcba
Urmat Bekboev <bek.urmat@gmail.com>
2019-05-25 12:22:38 +0600

    Initial Commit

---
+++ src/constants.js
@@ -0,0 +1,1 @@
+export let apiURL = 'http://localhost:8000';
---
+++ src/store/reducers/userReducer.js
@@ -0,0 +1,14 @@
+const initialState = {
+    user: null,
+    loginError: null,
+    registerError: null
+};
+
+const usersReducer = (state = initialState, action) => {
+    switch (action.type) {
+        default:
+            return state;
+    }
+};
+
+export default usersReducer;
