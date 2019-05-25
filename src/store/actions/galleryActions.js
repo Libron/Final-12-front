f63806a33667000faafbd74bdfe08ab4a2dac803
Urmat Bekboev <bek.urmat@gmail.com>
2019-05-25 14:54:16 +0600

    Upload and Delete actions created

--- src/store/actions/galleryActions.js
+++ src/store/actions/galleryActions.js
@@ -1,37 +1,67 @@
 import axios from '../../axios-api';
+import {push} from 'connected-react-router';
+import {NotificationManager} from "react-notifications";
 
 export const FETCH_PHOTOS_REQUEST = 'FETCH_PHOTOS_REQUEST';
 export const FETCH_PHOTOS_SUCCESS = 'FETCH_PHOTOS_SUCCESS';
 export const FETCH_PHOTOS_FAILURE = 'FETCH_PHOTOS_FAILURE';
 
-export const FETCH_PHOTO_REQUEST = 'FETCH_PHOTO_REQUEST';
-export const FETCH_PHOTO_SUCCESS = 'FETCH_PHOTO_SUCCESS';
-export const FETCH_PHOTO_FAILURE = 'FETCH_PHOTO_FAILURE';
+export const UPLOAD_PHOTO_REQUEST = 'FETCH_PHOTO_REQUEST';
+export const UPLOAD_PHOTO_SUCCESS = 'FETCH_PHOTO_SUCCESS';
+export const UPLOAD_PHOTO_FAILURE = 'FETCH_PHOTO_FAILURE';
+
+export const DELETE_PHOTO_REQUEST = 'DELETE_PHOTO_REQUEST';
+export const DELETE_PHOTO_SUCCESS = 'DELETE_PHOTO_SUCCESS';
+export const DELETE_PHOTO_FAILURE = 'DELETE_PHOTO_FAILURE';
 
 export const fetchPhotosRequest = () => ({type: FETCH_PHOTOS_REQUEST});
 export const fetchPhotosSuccess = photos => ({type: FETCH_PHOTOS_SUCCESS, photos});
 export const fetchPhotosFailure = error => ({type: FETCH_PHOTOS_FAILURE, error});
 
-export const fetchPhotoRequest = () => ({type: FETCH_PHOTO_REQUEST});
-export const fetchPhotoSuccess = photo => ({type: FETCH_PHOTO_SUCCESS, product});
-export const fetchPhotoFailure = error => ({type: FETCH_PHOTO_FAILURE, error});
+export const uploadPhotoRequest = () => ({type: UPLOAD_PHOTO_REQUEST});
+export const uploadPhotoSuccess = photo => ({type: UPLOAD_PHOTO_SUCCESS, photo});
+export const uploadPhotoFailure = error => ({type: UPLOAD_PHOTO_FAILURE, error});
 
-export const fetchPhotos = () => {
+export const deletePhotoRequest = () => ({type: DELETE_PHOTO_REQUEST});
+export const deletePhotoSuccess = photo => ({type: DELETE_PHOTO_SUCCESS, photo});
+export const deletePhotoFailure = error => ({type: DELETE_PHOTO_FAILURE, error});
+
+export const fetchPhotos = (query) => {
     return dispatch => {
         dispatch(fetchPhotosRequest());
-        return axios.get('/gallery').then(
+        let url = '/gallery';
+        if (query) {
+            url += query;
+        }
+        return axios.get(url).then(
             response => dispatch(fetchPhotosSuccess(response.data)),
             error => dispatch(fetchPhotosFailure(error))
         );
     };
 };
 
-export const fetchPhoto = id => {
+export const uploadPhoto = data => {
     return dispatch => {
-        dispatch(fetchPhotoRequest());
-        return axios.get('/gallery/' + id).then(
-            response => dispatch(fetchPhotoSuccess(response.data)),
-            error => dispatch(fetchPhotoFailure(error))
+        dispatch(uploadPhotoRequest());
+        return axios.post('/gallery', data).then(
+            response => {
+                dispatch(uploadPhotoSuccess(response.data));
+                dispatch(push('/'));
+            },
+            error => dispatch(uploadPhotoFailure(error))
+        )
+    }
+};
+
+export const deletePhoto = id => {
+    return dispatch => {
+        dispatch(deletePhotoRequest());
+        return axios.delete('/gallery/' + id).then(
+            response => {
+                NotificationManager.success('Photo removed');
+                dispatch(deletePhotoSuccess(response.data));
+            },
+            error => dispatch(deletePhotoFailure(error))
         );
     };
 };
--- src/store/reducers/galleryReducer.js
+++ src/store/reducers/galleryReducer.js
@@ -1,9 +1,7 @@
 import {
-    FETCH_PHOTO_FAILURE,
-    FETCH_PHOTO_REQUEST,
-    FETCH_PHOTO_SUCCESS, FETCH_PHOTOS_FAILURE,
+    FETCH_PHOTOS_FAILURE,
     FETCH_PHOTOS_REQUEST,
-    FETCH_PHOTOS_SUCCESS
+    FETCH_PHOTOS_SUCCESS, UPLOAD_PHOTO_FAILURE, UPLOAD_PHOTO_REQUEST, UPLOAD_PHOTO_SUCCESS
 } from "../actions/galleryActions";
 
 const initialState = {
@@ -16,13 +14,13 @@ const initialState = {
 const galleryReducer = (state = initialState, action) => {
     switch (action.type) {
         case FETCH_PHOTOS_REQUEST:
-        case FETCH_PHOTO_REQUEST:
+        case UPLOAD_PHOTO_REQUEST:
             return {...state, loading: true};
         case FETCH_PHOTOS_SUCCESS:
             return {...state, gallery: action.photos, loading: false};
-        case FETCH_PHOTO_SUCCESS:
+        case UPLOAD_PHOTO_SUCCESS:
             return {...state, photo: action.photo, loading: false};
-        case FETCH_PHOTO_FAILURE:
+        case UPLOAD_PHOTO_FAILURE:
         case FETCH_PHOTOS_FAILURE:
             return {...state, error: action.error};
         default:
