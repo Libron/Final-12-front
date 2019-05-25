4b3c85aefa920fc3115ac2f3873f198587968865
Urmat Bekboev <bek.urmat@gmail.com>
2019-05-25 12:59:42 +0600

    Login route added

--- src/Routes.js
+++ src/Routes.js
@@ -2,18 +2,13 @@ import React from 'react';
 import {Route, Switch} from "react-router-dom";
 import Register from "./containers/Register/Register";
 import Login from "./containers/Login/Login";
-import Cocktails from "./containers/Cocktails/Cocktails";
-import CocktailDetail from "./containers/CocktailDetail/CocktailDetail";
-import CocktailBuilder from "./containers/CocktailBuilder/CocktailBuilder";
+import Gallery from "./containers/Gallery/Gallery";
 
 const Routes = ({user}) => {
   return (
     <Switch>
-      <Route path="/" exact component={Cocktails} />
-      <Route path="/cocktails" exact component={Cocktails} />
-        <Route path="/cocktails/mylist" exact component={Cocktails} />
-        <Route path="/cocktails/create" exact component={CocktailBuilder} />
-        <Route path="/cocktails/:id" exact component={CocktailDetail} />
+      <Route path="/" exact component={Gallery} />
+      <Route path="/gallery" exact component={Gallery} />
       <Route path="/register" exact component={Register}/>
       <Route path="/login" exact component={Login}/>
     </Switch>
